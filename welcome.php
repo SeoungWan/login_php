15d3efa70d1a7173f3be7b0b8ef31b3dcee99714
Seungwan Han <qak8883@naver.com>
2024-06-07 10:00:20 +0900

    simple_logint_php

---
+++ welcome.php
@@ -0,0 +1,51 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
+</head>
+<body>
+    <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
+    <p>
+        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
+        <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
+    </p>
+</body>
+</html>
+
+
+③ 로그아웃 스크립트 만들기
+● 사용자가 로그아웃 또는 로그아웃 링크를 클릭하면 이 파일내의 스크립트가 세션을 삭제하고 사용자를 다시 로그인 페이지로 리디렉션한다.
+
+# cd /var/www/html
+# vi logout.php
+
+<?php
+// Initialize the session
+session_start();
+ 
+// Unset all of the session variables
+$_SESSION = array();
+ 
+// Destroy the session.
+session_destroy();
+ 
+// Redirect to login page
+header("location: login.php");
+exit;
+?>
